e959538a8ecaaeeb9ae1de15c2aadb11103f4419
Dmitrii Shapoval <shapoval.dmitrii.viktorovich@gmail.com>
2024-02-09 07:56:38 +0200

    Navigation connected

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import Posts from './src/containers/Posts';
 import {fas} from '@fortawesome/free-solid-svg-icons';
 import PostDetails from './src/containers/PostDetails';
 import {library} from '@fortawesome/fontawesome-svg-core';
+import {NavigationContainer} from '@react-navigation/native';
 library.add(fas);
 function App(): React.JSX.Element {
   return (
-    <>
+    <NavigationContainer>
       <PostDetails />
-    </>
+    </NavigationContainer>
   );
 }
 export default App;
